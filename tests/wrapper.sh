d5e65be53bca4ed67e15f00b9a62d8e7ca84d561
HAMKEE <hello@hamkee.net>
2026-05-18 12:41:31 +0000

    Fix wrapper option parsing and exit propagation

--- tests/wrapper.sh
+++ tests/wrapper.sh
@@ -8,7 +8,14 @@ fi
 while [ "$#" -gt 0 ]; do
     case "$1" in
         --policy-file)
+            if [ "$#" -lt 2 ]; then
+                echo "wrapper: --policy-file requires an argument" >&2
+                exit 2
+            fi
             shift 2
+            ;;
+        --policy-file=*)
+            shift
             ;;
         --)
             shift
